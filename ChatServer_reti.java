625e2b908172b99b83e5edfe4b301164a931aae0
Domenicomichele Silvestri <domenicomichele.silvestri@gmail.com>
2022-03-25 12:15:20 +0100

    Add files via upload

---
+++ ChatServer_reti.java
@@ -0,0 +1,131 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.*;
+import java.util.concurrent.Executors;
+
+/**
+ * A multithreaded chat room server. When a client connects the server requests
+ * a screen name by sending the client the text "SUBMITNAME", and keeps
+ * requesting a name until a unique one is received. After a client submits a
+ * unique name, the server acknowledges with "NAMEACCEPTED". Then all messages
+ * from that client will be broadcast to all other clients that have submitted a
+ * unique screen name. The broadcast messages are prefixed with "MESSAGE".
+ *
+ * This is just a teaching example so it can be enhanced in many ways, e.g.,
+ * better logging. Another is to accept a lot of fun commands, like Slack.
+ */
+public class ChatServer_reti {
+
+    // All client names, so we can check for duplicates upon registration.
+    private static Set<String> names = new HashSet<>();
+
+    // The set of all the print writers for all the clients, used for broadcast.
+    private static Map<PrintWriter, String> writers = new HashMap<>();
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("The chat server is running...");
+        var pool = Executors.newFixedThreadPool(500);
+        try (var listener = new ServerSocket(28993)) {
+            while (true) {
+                pool.execute(new Handler(listener.accept()));
+            }
+        }
+    }
+
+    /**
+     * The client handler task.
+     */
+    private static class Handler implements Runnable {
+        private String name;
+        private Socket socket;
+        private Scanner in;
+        private PrintWriter out;
+
+        /**
+         * Constructs a handler thread, squirreling away the socket. All the interesting
+         * work is done in the run method. Remember the constructor is called from the
+         * server's main method, so this has to be as short as possible.
+         */
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        /**
+         * Services this thread's client by repeatedly requesting a screen name until a
+         * unique one has been submitted, then acknowledges the name and registers the
+         * output stream for the client in a global set, then repeatedly gets inputs and
+         * broadcasts them.
+         */
+        public void run() {
+            try {
+                in = new Scanner(socket.getInputStream());
+                out = new PrintWriter(socket.getOutputStream(), true);
+
+                // Keep requesting a name until we get a unique one.
+                while (true) {
+                    out.println("SUBMITNAME");
+                    name = in.nextLine();
+                    if (name == null) {
+                        return;
+                    }
+                    synchronized (names) {
+                        if (!name.isBlank() && !names.contains(name)) {
+                            names.add(name);
+                            break;
+                        }
+                    }
+                }
+
+                // Now that a successful name has been chosen, add the socket's print writer
+                // to the set of all writers so this client can receive broadcast messages.
+                // But BEFORE THAT, let everyone else know that the new person has joined!
+                out.println("NAMEACCEPTED " + name);
+                for (Map.Entry<PrintWriter, String> writer : writers.entrySet()) {
+                    writer.getKey().println("MESSAGE " + name + " has joined");
+                    writer.getKey().println("JOIN " + name);
+                }
+                writers.put(out,name);
+
+                // Accept messages from this client and broadcast them.
+                while (true) {
+                    // gestire qui chiamata richiesta lista amici oppure messaggio
+                    String input = in.nextLine();
+                    if (input.toLowerCase().startsWith("/quit")) {
+                        return;
+                    }
+                    if (input.toLowerCase().startsWith("getuserlist")) {
+                        for (Map.Entry<PrintWriter, String> writer : writers.entrySet()) {
+                            if (writer.getValue() == name){
+                                writer.getKey().println("LIST " + writers.values());
+                            }
+                        }
+                    } else {
+                        for (Map.Entry<PrintWriter, String> writer : writers.entrySet()) {
+                            writer.getKey().println("MESSAGE " + name + ": " + input);
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+            } finally {
+                if (out != null) {
+                    writers.remove(out);
+                }
+                if (name != null) {
+                    System.out.println(name + " is leaving");
+                    names.remove(name);
+                    for (Map.Entry<PrintWriter, String> writer : writers.entrySet()) {
+                        writer.getKey().println("MESSAGE " + name + " has left");
+                        writer.getKey().println("EXT " + name);
+                    }
+                }
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+    }
+}
